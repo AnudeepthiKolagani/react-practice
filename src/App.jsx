f3584b5e5f1aacbc32883396e27a319274b133db
Anudeepthi <anudeepthi.kolagani@gmail.com>
2026-06-26 20:39:08 +0530

    feat: create sepearte main layout with header for react tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import "./App.css";
-import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className="mx-20 my-5">
-      <Header />
     </div>
   );
 }
--- src/layout/Main.Layout.jsx
+++ src/layout/Main.Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header";
+import { Header } from "../components/headers/Header";
 
 export const MainLayout = () => {
   return (
